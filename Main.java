c84b25f7eec654882a04749bf2b13d1656c8bc78
sooho7382 <sooho7382@gmail.com>
2025-03-25 00:51:12 +0900

    last

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("정수호"); // 자신의 이름 사용
+        System.out.println("홍길동"); // 자신의 이름 사용
         World.run();
         World.run2();
+        Hello.run(); // 마지막 수정
     }
 }
