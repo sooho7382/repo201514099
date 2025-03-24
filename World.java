de4571f00283256abc54a54784a53d21abbedf99
sooho7382 <sooho7382@gmail.com>
2025-03-25 00:37:56 +0900

    fifth

--- World.java
+++ World.java
@@ -2,4 +2,8 @@ public class World {
     public static void run() {
         System.out.println("201514099"); // 자신의 학번 사용
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
